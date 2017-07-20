36df38106f08e4d456534ed0917b685536f25921
matheusps <matheus.ps@icloud.com>
2017-07-20 11:33:57 -0300

    improved schedule table, created search bar definitions, created form global definitions

---
+++ app/components/dashboard/shared/schedule-table/schedule-table.controller.js
@@ -0,0 +1,12 @@
+/*jshint strict:false */
+
+angular.module('MyApp').controller('ScheduleTableController', function($scope) {
+
+    //Columns
+    $scope.days = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'];
+
+    //Lines
+    $scope.hours = ['5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30',
+    '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30',
+    '17:00', '17:30', '18:00', '18:30', '19:00'];
+});
--- app/components/dashboard/shared/schedule-table/schedule-table.directive.js
+++ app/components/dashboard/shared/schedule-table/schedule-table.directive.js
@@ -1,5 +1,6 @@
 angular.module('MyApp').directive('scheduleTable', function() {
     return {
-        templateUrl: 'components/dashboard/shared/schedule-table/schedule-table.html'
+        templateUrl: 'components/dashboard/shared/schedule-table/schedule-table.html',
+        controller: 'ScheduleTableController'
     };
 });
