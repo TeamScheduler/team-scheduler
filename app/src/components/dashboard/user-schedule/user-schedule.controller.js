9e2b3c51a3ab291ccee035b6a60f554a5e930650
matheusps <matheus.ps@icloud.com>
2017-08-05 20:29:01 -0300

    added background to dashboard, added ts-logo and changed user-table

--- app/src/components/dashboard/user-schedule/user-schedule.controller.js
+++ app/src/components/dashboard/user-schedule/user-schedule.controller.js
@@ -1,7 +1,8 @@
 /*jshint strict:false */
 
-angular.module('MyApp').controller('UserScheduleController', function($scope) {
+angular.module('MyApp').controller('UserScheduleController', function($scope, $rootScope) {
 
+    $scope.user = $rootScope.currentUser;
     //Columns
     $scope.days = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'];
 
@@ -9,270 +10,428 @@ angular.module('MyApp').controller('UserScheduleController', function($scope) {
     $scope.hours =[
          {
              hour: '5:30',
-             mon: [],
-             tue: [],
-             wed: [],
-             thu: [],
-             fri: [],
-             sat: [],
-             sun: []
+             mon: false,
+             tue: false,
+             wed: false,
+             thu: false,
+             fri: false,
+             sat: false,
+             sun: false
         },
         {
             hour: '6:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '6:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '7:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '7:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '8:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '8:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '9:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '9:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '10:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '10:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '11:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '11:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '12:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '12:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '13:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '13:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '14:00',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         },
         {
             hour: '14:30',
-            mon: [],
-            tue: [],
-            wed: [],
-            thu: [],
-            fri: [],
-            sat: [],
-            sun: []
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '15:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '15:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '16:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '16:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '17:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '17:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '18:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '18:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '19:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '19:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '20:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '20:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '21:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '21:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '22:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '22:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '23:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '23:30',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+        {
+            hour: '00:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
+        },
+
+        {
+            hour: '1:00',
+            mon: false,
+            tue: false,
+            wed: false,
+            thu: false,
+            fri: false,
+            sat: false,
+            sun: false
         }
     ];
 
     $scope.isEditMode = false;
 
-    $scope.addHour = function (hour, dayFlag) {
+    $scope.editTableHour = function (hour, dayFlag) {
 
         if($scope.isEditMode){
             temp = $scope.hours[getIndex(hour)];
 
             if(dayFlag === 'mon')
-                temp.mon.push('USERNAME!');
+                temp.mon = !temp.mon;
 
             if(dayFlag === 'tue')
-                temp.tue.push('USERNAME!');
+                temp.tue = !temp.tue;
 
             if(dayFlag === 'wed')
-                temp.wed.push('USERNAME!');
+                temp.wed = !temp.wed;
 
             if(dayFlag === 'thu')
-                temp.thu.push('USERNAME!');
+                temp.thu = !temp.thu;
 
             if(dayFlag === 'fri')
-                temp.fri.push('USERNAME!');
+                temp.fri = !temp.fri;
 
             if(dayFlag === 'sat')
-                temp.sat.push('USERNAME!');
+                temp.sat = !temp.sat;
 
             if(dayFlag === 'sun')
-                temp.sun.push('USERNAME!');
+                temp.sun = !temp.sun;
         }
     };
 
     function getIndex(hour) {
       return $scope.hours.indexOf(hour);
-    };
-
-    $scope.removeHour = function (hour, dayFlag, user) {
-
-        if($scope.isEditMode){
-
-            temp = $scope.hours[getIndex(hour)];
-
-            if(dayFlag === 'mon'){
-                var index = temp.mon.indexOf(user);
-                temp.mon.splice(index, 1);
-            }
-
-            if(dayFlag === 'tue'){
-                var index = temp.tue.indexOf(user);
-                temp.tue.splice(index, 1);
-            }
-
-            if(dayFlag === 'wed'){
-                var index = temp.wed.indexOf(user);
-                temp.wed.splice(index, 1);
-            }
-
-            if(dayFlag === 'thu'){
-                var index = temp.thu.indexOf(user);
-                temp.thu.splice(index, 1);
-            }
-
-            if(dayFlag === 'fri'){
-                var index = temp.fri.indexOf(user);
-                temp.fri.splice(index, 1);
-            }
-
-            if(dayFlag === 'sat'){
-                var index = temp.sat.indexOf(user);
-                temp.sat.splice(index, 1);
-            }
-
-            if(dayFlag === 'sun'){
-                var index = temp.sun.indexOf(user);
-                temp.sun.splice(index, 1);
-            }
-        }
     };
 });
